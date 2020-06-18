c689e7f6ff5514d14ba3ed5b35f8be90a963cba5
Filipe Mota <drete457@gmail.com>
2020-06-18 16:49:41 +0100

    Updates

---
+++ precache-manifest.a6ec76e3071e5354f34b75b29b32b7ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b09be92f2c30cf82e8e4b5fec545ec7",
+    "url": "/HeroPage/index.html"
+  },
+  {
+    "revision": "8e55f612a619c7dd9dc2",
+    "url": "/HeroPage/static/css/main.57e22a14.chunk.css"
+  },
+  {
+    "revision": "a92df05d2682865cab2d",
+    "url": "/HeroPage/static/js/2.bd095026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HeroPage/static/js/2.bd095026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e55f612a619c7dd9dc2",
+    "url": "/HeroPage/static/js/main.4ef5a021.chunk.js"
+  },
+  {
+    "revision": "3dfd9df99f6c92148332",
+    "url": "/HeroPage/static/js/runtime-main.3b81f9df.js"
+  },
+  {
+    "revision": "3d1042bd279c591944b1fa2eeba2620e",
+    "url": "/HeroPage/static/media/natsu.3d1042bd.png"
+  }
+]);
